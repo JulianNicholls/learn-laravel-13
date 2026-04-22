3ca0f5ad24fe68e2c49aa7f0ced642bfc3392aa2
Julian Nicholls <juliannicholls29@gmail.com>
2026-04-22 10:41:38 +0100

    Update ChirpController and Create chirps table migration

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
         <div class="card bg-base-100 shadow mt-8">
             <div class="card-body">
                 <div>
-                    <div class="font-semibold">{{  $chirp['author'] }}</div>
+                    <div class="font-semibold">{{ $chirp['author'] }}</div>
                     <div class="mt-1">{{ $chirp['message'] }}</div>
                     <div class="text-sm text-gray-500 mt-2">{{ $chirp['time'] }}</div>
                 </div>
