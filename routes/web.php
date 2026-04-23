264e0d4e1723ff1a1f2187f7d2888fb72601e3ef
Julian Nicholls <juliannicholls29@gmail.com>
2026-04-23 11:29:49 +0100

    Add login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\Auth\Register;
+use App\Http\Controllers\Auth\Login;
 use App\Http\Controllers\Auth\Logout;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,10 @@ Route::get('/', [ChirpController::class, 'index']);
 
 Route::view('/register', 'auth.register')->middleware('guest')->name('register');
 Route::post('/register', Register::class)->middleware('guest');
+
+Route::view('/login', 'auth.login')->middleware('guest')->name('login');
+Route::post('/login', Login::class)->middleware('guest');
+
 Route::post('/logout', Logout::class)->middleware('auth')->name('logout');
 
 Route::middleware('auth')->group(function () {
